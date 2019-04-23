56697cdaa1ed4d4a3860973d8958a2bae55603b6
thienhuu1804 <43635715+thienhuu1804@users.noreply.github.com>
2019-04-24 02:32:21 +0700

    Add files via upload

---
+++ KetNoiDB/access.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once 'connection.php';
+
+function sqlQuery($sql) {
+    $con = ketnoidb("localhost", "root", "", "shopgame");
+    mysqli_set_charset($con, 'UTF8');
+    $result = mysqli_query($con, $sql);
+    if ($result) {
+        return $result;
+    } else {
+        echo 'Du lieu rong';
+        return 0;
+    }
+}
+
+function getAllSanPham()
+{
+    $sql= "SELECT * FROM sanpham";
+    return sqlQuery($sql);
+}
+
+function updateSanPham($maSP , $tenSP ,$moTa  , $hinhAnh, $soLuong , $gia){
+    $sql= "UPDATE sanpham SET TenSP='".$tenSP."',HinhAnh='".$hinhAnh."',SoLuong='".$soLuong."',DonGia='".$gia."' WHERE MaSP='".$maSP."';";
+}
+//func
+?>
